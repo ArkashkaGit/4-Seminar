fee8443bd4a47071d4ed29ef264cc8f17aad3401
Arkashka <sevenkapsul@mail.ru>
2022-08-03 17:29:44 +0300

    задача_25

--- Job/25/Program.cs
+++ Job/25/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 25: 
+Напишите цикл, 
+который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+пример:
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+
+
+Console.WriteLine("Введите первое число -A-...");
+int firstNumber_A = int.Parse(Console.ReadLine());
+
+Console.WriteLine ("Введите второе число -B-...");
+int secondNumber_B = int.Parse(Console.ReadLine());
+
+int sqrt = 1;
+
+for (int i=0; i<secondNumber_B; i++)
+{
+sqrt = sqrt * firstNumber_A;
+}
+
+Console.WriteLine("Число -A- возведено в натуральную степень числа -B-..");
+Console.WriteLine(sqrt);
