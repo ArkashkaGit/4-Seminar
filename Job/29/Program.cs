5ded3107f4afe44028da5eb91bca36fba98239d0
Arkashka <sevenkapsul@mail.ru>
2022-08-03 20:03:05 +0300

    задача 29

--- Job/29/Program.cs
+++ Job/29/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/*
+Задача 29: 
+Напишите программу,
+которая задаёт массив из 8 элементов
+и выводит их на экран.
+
+Пример:
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]
+*/
+
+int[] massive = new int[8];
+
+for (int i = 0; i < massive.Length; i++)
+{
+    massive[i] = new Random().Next(0, 999);
+    int list = massive[i];
+    Console.Write(list + " ");
+}
